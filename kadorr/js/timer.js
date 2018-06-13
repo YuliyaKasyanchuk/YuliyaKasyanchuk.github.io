0f1e704daa990817c951565ca67c1fd3d3fc4d36
Yulja <yuljakas@gmail.com>
2018-06-13 16:59:29 +0300

    new

---
+++ kadorr/js/timer.js
@@ -0,0 +1,76 @@
+jQuery(document).ready(function($) {
+  var note = $('#note'),
+    ts = new Date(2017, 9, 21),
+    newYear = true;
+  
+  if((new Date()) > ts){
+    ts = (new Date()).getTime() + 10*24*60*60*1000;
+    newYear = false;
+  }
+    
+  $('#countdown').countdown({
+    timestamp : ts,
+    callback  : function(days, hours, minutes, seconds){
+      
+      var message = "";
+      
+      message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+      message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+      message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+      message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+      
+      if(newYear){
+        message += "left until the new year!";
+      }
+      else {
+        message += "left to 10 days from now!";
+      }
+      
+      note.html(message);
+    }
+  });
+  $('#countdown-2').countdown({
+    timestamp : ts,
+    callback  : function(days, hours, minutes, seconds){
+      
+      var message = "";
+      
+      message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+      message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+      message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+      message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+      
+      if(newYear){
+        message += "left until the new year!";
+      }
+      else {
+        message += "left to 10 days from now!";
+      }
+      
+      note.html(message);
+    }
+  });
+  $('#countdown-3').countdown({
+    timestamp : ts,
+    callback  : function(days, hours, minutes, seconds){
+      
+      var message = "";
+      
+      message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+      message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+      message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+      message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+      
+      if(newYear){
+        message += "left until the new year!";
+      }
+      else {
+        message += "left to 10 days from now!";
+      }
+      
+      note.html(message);
+    }
+  });
+
+});
+
