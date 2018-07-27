e72bebc2081cef7bf92b0b2e2a7483a6bc81fbd4
Yulja <yuljakas@gmail.com>
2018-07-27 15:14:18 +0300

    v-2

--- Test/dev/js/script.js
+++ Test/dev/js/script.js
@@ -213,3 +213,49 @@ smMenuItem.addEventListener('click', function(event) {
         }
     }
 });
+
+// SCROLL TOP
+
+   var scroll = document.querySelector('.scroll-top');
+
+    window.onscroll = function() {
+        var scrolled = window.pageYOffset;
+        if (scrolled > 50) {
+            scroll.style.transform = 'scale(1)';
+        } else {
+            scroll.style.transform = 'scale(0)';
+        }
+
+    }
+
+
+
+    scroll.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        function animate(draw, duration) {
+            let start = performance.now();
+            duration = duration || 1000;
+            requestAnimationFrame(function animate(time) {
+                let timePassed = time - start;
+
+                if (timePassed > duration) {
+                    timePassed = duration;
+                }
+                draw(timePassed);
+
+                if (timePassed < duration) {
+                    requestAnimationFrame(animate);
+
+                }
+            });
+
+        };
+
+        animate(function(timePassed) {
+            window.scrollBy(0, -window.pageYOffset / 20);
+        }, 2000);
+
+
+
+    });
